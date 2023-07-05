2054afe0ffdc30dcdecb2cd31cc6a568351e0f22
Ranju <ranjupillai2016@gmail.com>
2023-07-05 11:55:16 +0530

    Inital Code change

---
+++ Demo1_SpringBoot_Transaction/src/main/java/com/springboot/transaction/Demo1SpringBootTransactionApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1SpringBootTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo1SpringBootTransactionApplication.class, args);
+	}
+
+}
---
+++ Demo1_SpringBoot_Transaction/src/test/java/com/springboot/transaction/Demo1SpringBootTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1SpringBootTransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
